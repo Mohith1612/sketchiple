0b76ac245eca43730da03e507e3871c2e02fc39c
Mohith1612 <mohith.dev16@gmail.com>
2026-03-26 11:17:38 +0530

    feat(server): add sync message broadcast to Room

--- packages/server/src/rooms/Room.ts
+++ packages/server/src/rooms/Room.ts
@@ -2,6 +2,8 @@ import * as Y from 'yjs'
 import * as awarenessProtocol from 'y-protocols/awareness'
 import type { WebSocket } from 'uWebSockets.js'
 import type { UserData } from '../ws/handler.js'
+import { MSG_SYNC, MSG_AWARENESS } from '@canvas-draw/shared'
+import * as encoding from 'lib0/encoding'
 import { logger } from '../utils/logger.js'
 
 export class Room {
@@ -15,6 +17,12 @@ export class Room {
     this.doc = new Y.Doc()
     this.awareness = new awarenessProtocol.Awareness(this.doc)
     this.clients = new Set()
+
+    this.awareness.on('update', ({ added, updated, removed }: { added: number[]; updated: number[]; removed: number[] }) => {
+      const changedClients = [...added, ...updated, ...removed]
+      const update = awarenessProtocol.encodeAwarenessUpdate(this.awareness, changedClients)
+      this.broadcastAwareness(update)
+    })
   }
 
   addClient(ws: WebSocket<UserData>): void {
@@ -33,6 +41,11 @@ export class Room {
     return this.clients.size
   }
 
+  applyUpdate(update: Uint8Array, origin: WebSocket<UserData>): void {
+    Y.applyUpdate(this.doc, update, origin)
+    this.broadcast(encodeSyncUpdate(update), origin)
+  }
+
   broadcast(message: Uint8Array, exclude?: WebSocket<UserData>): void {
     for (const client of this.clients) {
       if (client === exclude) continue
@@ -44,10 +57,30 @@ export class Room {
     ws.send(message, true)
   }
 
+  broadcastAwareness(awarenessUpdate: Uint8Array): void {
+    const msg = encodeAwarenessMessage(awarenessUpdate)
+    this.broadcast(msg)
+  }
+
   destroy(): void {
     this.awareness.destroy()
     this.doc.destroy()
     this.clients.clear()
     logger.info('room destroyed', { roomId: this.roomId })
   }
+}
+
+function encodeSyncUpdate(update: Uint8Array): Uint8Array {
+  const enc = encoding.createEncoder()
+  encoding.writeVarUint(enc, MSG_SYNC)
+  encoding.writeVarUint(enc, 2)
+  encoding.writeVarUint8Array(enc, update)
+  return encoding.toUint8Array(enc) as Uint8Array
+}
+
+function encodeAwarenessMessage(update: Uint8Array): Uint8Array {
+  const enc = encoding.createEncoder()
+  encoding.writeVarUint(enc, MSG_AWARENESS)
+  encoding.writeVarUint8Array(enc, update)
+  return encoding.toUint8Array(enc) as Uint8Array
 }
