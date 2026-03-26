7741bda31598e1d90b09786f4164fa3e69765282
Mohith1612 <mohith.dev16@gmail.com>
2026-03-26 15:54:42 +0530

    feat(server): wire WebSocket handler into HTTP server

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,12 @@
 import uWS from 'uWebSockets.js'
+import { attachWebSocketHandler } from './ws/handler.js'
 import { logger } from './utils/logger.js'
 
 const PORT = Number(process.env.PORT ?? 3001)
 
 const app = uWS.App()
+
+attachWebSocketHandler(app)
 
 app.get('/health', (res) => {
   res.writeHeader('Content-Type', 'application/json').end(JSON.stringify({ ok: true }))
